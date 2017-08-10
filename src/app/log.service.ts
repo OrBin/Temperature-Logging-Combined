c0c50671f3afba736f6c97a9f16a86e12f2107d6
Or Bin <orbin50@gmail.com>
2017-08-10 10:44:48 +0300

    Made LogService get data from server

--- src/app/log.service.ts
+++ src/app/log.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Log } from './log';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class LogService {
+
+  private logsUrl = '/latest';
+
+  constructor(private http: Http) { }
+
   getLatestLogs(): Promise<Log[]> {
-    return Promise.resolve([] as Log[]);
+    return this.http.get(this.logsUrl)
+                    .toPromise()
+                    .then(response => response.json() as Log[])
+                    .catch(this.handleError);
   }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
+
 }
