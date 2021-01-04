419dea2b60f66c10a2446b79e7a8dc2953a138be
Or Bin <orbin50@gmail.com>
2021-01-04 10:31:22 +0200

    Added CORS

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import fastapi
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
 
 api = fastapi.FastAPI()
 api.include_router(router)
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == '__main__':
     uvicorn.run(api)
